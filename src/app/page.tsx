be6702a79f87879bafabf43470ceb60752b106e5
Oussama Dakhch <odakhch@e1r8p14.1337.ma>
2023-12-09 18:31:45 +0100

    implement back to top button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import DisplayComponent from './Components/DisplayComponent'
+import ScroolToTop from './Components/ScrollToTop'
 
 export default function Home() {
 
@@ -6,7 +7,8 @@ export default function Home() {
   return (
     <>
       <main>
-      <DisplayComponent></DisplayComponent>
+        <ScroolToTop></ScroolToTop>
+        <DisplayComponent></DisplayComponent>
       </main>
     </>
   )
